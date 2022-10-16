7923abeab2ba4300bc882b355b79ac6110a4b138
Sujay509 <sujaychauhan98@gmail.com>
2022-10-16 16:18:17 +0530

    Welcome to Address Book System

---
+++ src/com/bridgelabz/addressbook/Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(" Welcome to Address Book System ");
+	}
+
+}
