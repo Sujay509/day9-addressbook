18de188d6a95bd8a166596166ba652934f013037
Sujay509 <sujaychauhan98@gmail.com>
2022-10-16 17:14:28 +0530

    Refactored the code to Add New Contact

---
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		  creating object of AddressBook class
+		  calling addContact method and displayContact
+		 */
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact();
+		addressBook.displayContact();
+	}
+}
+
+
